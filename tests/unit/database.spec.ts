9a79a3c24f660d06a8bb9b16e0dc27d18b68f8a8
Oliver Emery <thrymgjol@gmail.com>
2019-09-10 19:11:59 -0500

    Updated database code and unit tests for better+cleaner mocking.

--- tests/unit/database.spec.ts
+++ tests/unit/database.spec.ts
@@ -1,37 +1,19 @@
-const fs = require("fs")
+import { Chat, ChatStore, StorageEngine } from "@app/database"
 
-import { ChatStore } from "@app/database"
+class MockStorageEngine implements StorageEngine {
+  public write: (chats: Chat[]) => void
+  public read: () => Chat[]
 
-jest.mock("@app/config", () => ({
-  data_directory: "tmp",
-}))
-
-jest.mock("fs", () => ({
-  existsSync: jest.fn(),
-  readFileSync: jest.fn(),
-  writeFileSync: jest.fn(),
-}))
-
-const databaseFile = "test-database.json"
-
-function prepareMockDatabaseFile(content?: string) {
-  if (content) {
-    fs.existsSync.mockReturnValue(true)
-    fs.readFileSync.mockReturnValue(content)
-  } else {
-    fs.existsSync.mockReturnValue(false)
-    fs.readFileSync.mockImplementation(() => {
-      throw Error()
-    })
+  public constructor(chats?: Chat[]) {
+    this.write = jest.fn()
+    this.read = jest.fn().mockReturnValue(chats ? chats : [])
   }
 }
 
 describe("ChatStore", () => {
   describe("getRepositoriesByChat()", () => {
     it("return an empty list for no matches", () => {
-      prepareMockDatabaseFile()
-
-      const store = new ChatStore(databaseFile)
+      const store = new ChatStore(new MockStorageEngine())
       expect(store.getRepositoriesByChat("test-chat")).toEqual([])
     })
 
@@ -39,58 +21,51 @@ describe("ChatStore", () => {
       const chat = "test-chat"
       const repos = ["A", "B", "C"]
 
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          {
-            chat_url: chat,
-            repositories: repos,
-          },
-        ])
-      )
+      const db = [
+        {
+          chat_url: chat,
+          repositories: repos,
+        },
+      ]
 
-      const store = new ChatStore(databaseFile)
+      const store = new ChatStore(new MockStorageEngine(db))
       expect(store.getRepositoriesByChat(chat)).toEqual(repos)
     })
   })
 
   describe("getChatsByRepository()", () => {
     it("should return an empty list for no matches", () => {
-      prepareMockDatabaseFile()
-
-      const store = new ChatStore(databaseFile)
+      const store = new ChatStore(new MockStorageEngine())
       expect(store.getChatsByRepository("test-repo")).toEqual([])
     })
 
     it("should return all chats that contain the repository", () => {
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          { chat_url: "A", repositories: ["1", "2", "3"] },
-          { chat_url: "B", repositories: ["1", "3"] },
-          { chat_url: "C", repositories: ["2"] },
-        ])
-      )
+      const db = [
+        { chat_url: "A", repositories: ["1", "2", "3"] },
+        { chat_url: "B", repositories: ["1", "3"] },
+        { chat_url: "C", repositories: ["2"] },
+      ]
 
-      const store = new ChatStore(databaseFile)
+      const store = new ChatStore(new MockStorageEngine(db))
       expect(store.getChatsByRepository("2").sort()).toEqual(["A", "C"])
     })
   })
 
   describe("renameRepository()", () => {
     it("should rename all occurrences of the target", () => {
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          { chat_url: "A", repositories: ["1", "2", "3"] },
-          { chat_url: "B", repositories: ["1", "3"] },
-          { chat_url: "C", repositories: ["2"] },
-        ])
-      )
+      const db = [
+        { chat_url: "A", repositories: ["1", "2", "3"] },
+        { chat_url: "B", repositories: ["1", "3"] },
+        { chat_url: "C", repositories: ["2"] },
+      ]
 
-      const store = new ChatStore(databaseFile)
+      const storageEngine = new MockStorageEngine(db)
+      const store = new ChatStore(storageEngine)
       expect(store.getChatsByRepository("3").sort()).toEqual(["A", "B"])
       expect(store.getChatsByRepository("4")).toEqual([])
 
       store.renameRepository("3", "4")
-      expect(fs.writeFileSync).toHaveBeenCalled()
+      expect(storageEngine.write).toHaveBeenCalled()
       expect(store.getChatsByRepository("3")).toEqual([])
       expect(store.getChatsByRepository("4").sort()).toEqual(["A", "B"])
     })
@@ -98,16 +73,15 @@ describe("ChatStore", () => {
 
   describe("addRepositoryToChat()", () => {
     it("should create a new entry if it does not exist already", () => {
-      prepareMockDatabaseFile()
-
       const chat = "test-chat"
       const repo = "test-repo"
 
-      const store = new ChatStore(databaseFile)
+      const storageEngine = new MockStorageEngine()
+      const store = new ChatStore(storageEngine)
 
       expect(store.getRepositoriesByChat(chat)).toHaveLength(0)
       store.addRepositoryToChat(repo, chat)
-      expect(fs.writeFileSync).toHaveBeenCalled()
+      expect(storageEngine.write).toHaveBeenCalled()
 
       let chatRepos = store.getRepositoriesByChat(chat)
       expect(chatRepos.length).toEqual(1)
@@ -118,17 +92,16 @@ describe("ChatStore", () => {
       const chat = "test-chat"
       const repos = ["A", "B", "C"]
 
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          {
-            chat_url: chat,
-            repositories: [repos[0], repos[1]],
-          },
-        ])
-      )
-      const store = new ChatStore(databaseFile)
+      const db = [
+        {
+          chat_url: chat,
+          repositories: [repos[0], repos[1]],
+        },
+      ]
+      const storageEngine = new MockStorageEngine(db)
+      const store = new ChatStore(storageEngine)
       store.addRepositoryToChat(repos[2], chat)
-      expect(fs.writeFileSync).toHaveBeenCalled()
+      expect(storageEngine.write).toHaveBeenCalled()
 
       let chatRepos = store.getRepositoriesByChat(chat)
       expect(chatRepos.length).toEqual(3)
@@ -138,58 +111,52 @@ describe("ChatStore", () => {
 
   describe("removeRepositoriesFromChat()", () => {
     it("should do nothing if chat does not exist", () => {
-      prepareMockDatabaseFile()
-      const store = new ChatStore(databaseFile)
+      const store = new ChatStore(new MockStorageEngine())
       expect(() => store.removeRepositoryFromChat("repo", "chat")).not.toThrow()
     })
 
     it("should do nothing if chat isn't subscribed to repo", () => {
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          {
-            chat_url: "chat",
-            repositories: ["A"],
-          },
-        ])
-      )
-      const store = new ChatStore(databaseFile)
+      const db = [
+        {
+          chat_url: "chat",
+          repositories: ["A"],
+        },
+      ]
+      const store = new ChatStore(new MockStorageEngine(db))
 
       store.removeRepositoryFromChat("B", "chat")
       expect(store.getRepositoriesByChat("chat")).toEqual(["A"])
     })
 
     it("should remove repository from subscribed chat", () => {
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          {
-            chat_url: "chat",
-            repositories: ["A", "B", "C"],
-          },
-        ])
-      )
-      const store = new ChatStore(databaseFile)
+      const db = [
+        {
+          chat_url: "chat",
+          repositories: ["A", "B", "C"],
+        },
+      ]
+      const storageEngine = new MockStorageEngine(db)
+      const store = new ChatStore(storageEngine)
 
       store.removeRepositoryFromChat("B", "chat")
-      expect(fs.writeFileSync).toHaveBeenCalled()
+      expect(storageEngine.write).toHaveBeenCalled()
       expect(store.getRepositoriesByChat("chat").sort()).toEqual(["A", "C"])
     })
   })
 
   describe("deleteRepository()", () => {
     it("should remove all occurrences of the repository", () => {
-      prepareMockDatabaseFile(
-        JSON.stringify([
-          { chat_url: "A", repositories: ["1", "2", "3"] },
-          { chat_url: "B", repositories: ["1", "3"] },
-          { chat_url: "C", repositories: ["2"] },
-        ])
-      )
-
-      const store = new ChatStore(databaseFile)
+      const db = [
+        { chat_url: "A", repositories: ["1", "2", "3"] },
+        { chat_url: "B", repositories: ["1", "3"] },
+        { chat_url: "C", repositories: ["2"] },
+      ]
+      const storageEngine = new MockStorageEngine(db)
+      const store = new ChatStore(storageEngine)
       expect(store.getChatsByRepository("3").sort()).toEqual(["A", "B"])
 
       store.deleteRepository("3")
-      expect(fs.writeFileSync).toHaveBeenCalled()
+      expect(storageEngine.write).toHaveBeenCalled()
       expect(store.getChatsByRepository("3")).toEqual([])
     })
   })
